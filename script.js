eb294ce3b59dd3bbf2293034e1b61c48f2d86d8b
vivek chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-04-24 15:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	let num1 = 0;
+	let num2 = 1;
+    let coumt = 0;
+
+	while(count < num){
+		console.log(num + " ");
+
+		let num3 = num1 + num2;
+		num1 = num2;
+		num2 = num3;
+		count++;
+	}
 }
 
-module.exports = fibonacci;
+module.exports = fibonacci(10);

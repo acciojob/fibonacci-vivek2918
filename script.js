ae3621e53d416b565028528ad61e9611a71b002c
vivek chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-04-24 16:55:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function fibonacci(num) {
 // your code here
-	if(num <= 1) return n;
-	return fibonacci(n-1)+fibonacci(n-2);
+	if (num === 0) {
+    return 0;
+  } else if (num === 1 || num === 2) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 }
 
 module.exports = fibonacci;
